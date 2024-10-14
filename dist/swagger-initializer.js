90fa2e1e606d4d4e0e485239d0828eb38196008c
Sebastian <141459503+sebastian-dang@users.noreply.github.com>
2024-10-14 17:05:11 +0000

    Remove extra files

--- dist/swagger-initializer.js
+++ dist/swagger-initializer.js
@@ -3,7 +3,7 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "contracts/events.swagger.yml",
+    url: "swagger.yml",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
